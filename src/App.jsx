52c127db2f6798fb8fe092b7f5b684c56a890c3a
isha-web1 <isha.web1@gmail.com>
2024-12-27 23:07:34 +0600

    set layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './components/blogs/Blogs'
+import BooksMarks from './components/booksmark/Booksmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='md : flex'>
       <Blogs></Blogs>
+      <BooksMarks></BooksMarks>
+      </div>
       
     </>
   )
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -9,8 +9,8 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     },[])
     return (
-        <div>
-            
+        <div className='md:w-2/3'>
+           <h1 className="text-4xl">Here is blogs section</h1> 
         </div>
     );
 };
